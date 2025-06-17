f4994c841f4af269516e7b05d72bfc878a544b4a
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-06-17 11:48:04 +0100

    exercise on logial operation

---
+++ exercism/vehicle-purchase/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+ption1 : option2;
+  return `${carBrand} is clearly the better choice.`;
+}
+
+/**
+ * Calculates an estimate for the price of a used vehicle in the dealership
+ * based on the original price and the age of the vehicle.
+ *
+ * @param {number} originalPrice
+ * @param {number} age
+ * @returns {number} expected resell price in the dealership
